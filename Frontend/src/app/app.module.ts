5e17dcd8d5c5433d1f30c5e6e780fa7c2e91d4eb
adri <62965634+cuadriante@users.noreply.github.com>
2023-04-20 23:18:58 -0600

    Add Login, Lobby, Game components

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LobbyComponent } from './components/lobby/lobby.component';
+import { GameComponent } from './components/game/game.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LobbyComponent,
+    GameComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
