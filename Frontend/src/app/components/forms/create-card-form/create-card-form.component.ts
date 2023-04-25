dbfc54c771e07cf7d88f969bb983aceb54611fe3
adri <62965634+cuadriante@users.noreply.github.com>
2023-04-24 23:57:15 -0600

    Add: create card initial api integration

--- Frontend/src/app/components/forms/create-card-form/create-card-form.component.ts
+++ Frontend/src/app/components/forms/create-card-form/create-card-form.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CardInt } from '../../interfaces/card.interface';
+import { Router } from '@angular/router'; 
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+
 
 @Component({
   selector: 'app-create-card-form',
@@ -7,8 +11,36 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-card-form.component.scss']
 })
 export class CreateCardFormComponent {
+  //Variables del backend
+  name !: CardInt;
+  types !: string[];
+  fault!:boolean;
 
-  constructor(private router: Router) {}
+
+  //Objetos de input del frontend
+  characterName = new FormControl('',[Validators.required]);
+  description = new FormControl('',[Validators.required]);
+  race = new FormControl('',[Validators.required]);
+  energy = new FormControl('',[Validators.required]);
+  price = new FormControl('',[Validators.required]);
+  type = new FormControl('',[Validators.required]);
+
+
+  constructor(private router: Router, private _formBuilder: FormBuilder,private api:ApiService) {}
+  
+  //La idea es que este módul genera el mensaje de error
+  getErrMessage(component:FormControl){
+    if(component.hasError('required')){//El usuario no escribió nada
+      return "Este campo es obligatorio."
+    }else if(component.value.length>30){//La contraseña no cuenta con la longitud requerida
+      return "Las contraseña debe tener un tamaño de 8 caracteres"
+    }else if(component.invalid){//El usuario se pasa de la cantidad de caracteres
+      return "El usuario debe tener entre 1 y 30 caracteres"}
+    else{
+      return ""
+    }
+  }
+  
   goToLobby(){
 
     this.router.navigate(['/lobby']);
---
+++ Frontend/src/app/components/interfaces/card.interface.ts
@@ -0,0 +1,8 @@
+export interface CardInt{
+    characterName:string|null;
+    description:string|null;
+    race:string|null;
+    energy:string|null;
+    price:string|null;
+    type:string|null;
+}
