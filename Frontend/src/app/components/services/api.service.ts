82b6722586db724da36a5315862ffa2ef77d86aa
MarceloTM-2001 <62965036+MarceloTM-2001@users.noreply.github.com>
2023-04-25 18:55:28 -0600

    apdate: API ports woth frontend to satisfy current needs for Sprint 1

--- Frontend/src/app/components/services/api.service.ts
+++ Frontend/src/app/components/services/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { AccountInt } from "../interfaces/account.interface";
 import { ResponseI } from "../interfaces/response.interface";
+import { CardInt } from "../interfaces/card.interface";
 import { HttpClient,HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 
@@ -13,13 +14,27 @@ export class ApiService{
 
     constructor(private http:HttpClient){}
     
-    //registerAccount(player:AccountInt):Observable<ResponseI>{
-    registerAccount(player:AccountInt){
-        let dir = this.url + "Direccion"
+    registerAccount(player:AccountInt):Observable<ResponseI>{
+        let dir = this.url + ""
         console.log("dir: "+ dir)
         console.log(player)
-        //return this.http.post<ResponseI>(dir,player)
-        return player
+        return this.http.post<ResponseI>(dir,player)
+    }
+    addCard(card:CardInt):Observable<ResponseI>{
+        let dir =this.url + "Dirección 2"
+        console.log("dir: "+ dir)
+        console.log(card)
+        return this.http.post<ResponseI>(dir,card)
+    }
+
+    getAllCards():Observable<CardInt[]>{
+        let dir = this.url + "Dirección 3"
+        return this.http.get<CardInt[]>(dir)
+      }
+
+    getCountries():Observable<string[]>{
+        let dir = this.url + "Dirección 4"
+        return this.http.get<string[]>(dir)
     }
 
 }
