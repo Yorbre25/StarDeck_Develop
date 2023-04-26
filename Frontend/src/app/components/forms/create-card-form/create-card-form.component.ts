cf39cf7d79ba56023121f117b2018f975e7c3561
adri <62965634+cuadriante@users.noreply.github.com>
2023-04-25 19:56:19 -0600

    Update: create card form

--- Frontend/src/app/components/forms/create-card-form/create-card-form.component.ts
+++ Frontend/src/app/components/forms/create-card-form/create-card-form.component.ts
@@ -12,7 +12,7 @@ import { ApiService } from '../../services/api.service';
 })
 export class CreateCardFormComponent {
   //Variables del backend
-  name !: CardInt;
+  card !: CardInt;
   types !: string[];
   fault!:boolean;
 
@@ -24,10 +24,12 @@ export class CreateCardFormComponent {
   energy = new FormControl('',[Validators.required]);
   price = new FormControl('',[Validators.required]);
   type = new FormControl('',[Validators.required]);
-
+  image = new FormControl('',[Validators.required]);
 
   constructor(private router: Router, private _formBuilder: FormBuilder,private api:ApiService) {}
   
+
+
   //La idea es que este módul genera el mensaje de error
   getErrMessage(component:FormControl){
     if(component.hasError('required')){//El usuario no escribió nada
@@ -42,9 +44,35 @@ export class CreateCardFormComponent {
   }
   
   goToLobby(){
+    if(this.characterName.invalid||this.description.invalid||this.race.invalid||this.energy.invalid||this.price.invalid||this.type.invalid||this.image.invalid){
+      this.fault=true
+    }else{
+      this.card.name = this.characterName.value
+      this.card.description = this.description.value
+      this.card.race = this.race.value
+      this.card.energy = this.energy.value
+      this.card.price = this.price.value
+      this.card.type = this.type.value
+
+      this.api.addCard(this.card)//acá llama a la API
 
     this.router.navigate(['/lobby']);
 
+  }}
+
+  ngOnInit(){
+    this.card = 
+    { name: "Nombre del Personaje",
+      image:"https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG",
+      energy:"100",
+      price:"1000",
+      type:"MR",
+      race:"Cat",
+      description: "Nyan Cat, or Pop-Tart Cat, refers to a cartoon cat with a Pop-Tart body and a rainbow behind it, flying through space, set to the tune of a Japanese pop song." };
+    //this.nationalities=this.api.getCountries()
+    this.types=["MR","SR","SSR","UR"]
+
+    this.fault=false
   }
 
 
