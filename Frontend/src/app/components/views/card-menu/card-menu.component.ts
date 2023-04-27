8f00798cb1c082a605412f18b2213d25cf2f7b4b
adri <62965634+cuadriante@users.noreply.github.com>
2023-04-26 22:37:00 -0600

    Update: card menu interface

--- Frontend/src/app/components/views/card-menu/card-menu.component.ts
+++ Frontend/src/app/components/views/card-menu/card-menu.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { CardInt } from '../../interfaces/card.interface';
 @Component({
   selector: 'app-card-menu',
   templateUrl: './card-menu.component.html',
   styleUrls: ['./card-menu.component.scss']
 })
-export class CardMenuComponent {
+export class CardMenuComponent implements OnInit{
+  cards!:CardInt[];
 
+  constructor(private api: ApiService) {
+      
+    //console.log(this.cards)
+  }
+
+  ngOnInit(): void {
+    this.api.getAllCards().subscribe(data => {
+      console.log(data)
+      this.cards = data 
+    });
+  }
 }
