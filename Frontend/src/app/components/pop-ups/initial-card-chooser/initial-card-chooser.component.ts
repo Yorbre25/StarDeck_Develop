30c06aac6905ef213d6cea3e641750fca0b6cc09
MarceloTM-2001 <62965036+MarceloTM-2001@users.noreply.github.com>
2023-04-27 07:22:45 -0600

    fix: minor details regarding filtering of initial process

--- Frontend/src/app/components/pop-ups/initial-card-chooser/initial-card-chooser.component.ts
+++ Frontend/src/app/components/pop-ups/initial-card-chooser/initial-card-chooser.component.ts
@@ -4,6 +4,8 @@ import { MatDialogActions } from '@angular/material/dialog';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import { ApiService } from '../../services/api.service';
 import { CardInt } from '../../interfaces/card.interface';
+import { LoginService } from '../../services/login.service';
+import { seleced_Card_S } from '../../services/selected_card.service';
 
 /**
  * @description 
@@ -31,53 +33,69 @@ export class InitialCardChooserComponent implements OnInit{
 
   cards!:CardInt[];
 
- // clickableCards!:CardInt[];
-
-  ngOnInit(): void {
-    this.api.getAllCards().subscribe(data => {
-      console.log(data)
-      this.cards = data 
-    });
-  }
-
-
-
-    constructor(private dialogRef: MatDialogRef<InitialCardChooserComponent>, private api: ApiService) {
-      
-      //console.log(this.cards)
-    }
-
-    close() {
-      this.dialogRef.close();
-  }
-   clickableCards: CardInt[] = [
+  clickableCards: CardInt[] = [
+    
     { id:'',
-      name: "Nyan Cat",
-      image:"https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG",
-      energy:100,
-      cost:500,
-      type:"UR",
-      race:"Nyan",
-      description: "Nyanyanyanyanyanyanya!",
-      activated_card:true}, 
-    { id:'',
-    name: "Mametchi",
-    image: "https://tamagotchi.com/wp-content/uploads/mametchi.jpg",
-    energy:30,
-    cost:500,
-    type:"SSR",
-    race:"Tamagotchi",
-    description: "Mametchi loves inventing things and though sometimes he fails he will succeed, he just keeps trying. He loves to study and play sports.",
-    activated_card:true },
-    { id:'',
-    name: "Ginjirotchi",
-    image:"https://tamagotchi.com/wp-content/uploads/ginjirotchi.jpg",
+    name: "Nyan Cat",
+    image:"https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG",
     energy:100,
     cost:500,
     type:"UR",
     race:"Nyan",
-    description: "Ginjirotchi is cheerful and full of energy, though also compassionate. He loves watching dramatic movies.",
-    activated_card:true}]
+    description: "Nyanyanyanyanyanyanya!",
+    activated_card:true}, 
+  { id:'',
+  name: "Mametchi",
+  image: "https://tamagotchi.com/wp-content/uploads/mametchi.jpg",
+  energy:30,
+  cost:500,
+  type:"SSR",
+  race:"Tamagotchi",
+  description: "Mametchi loves inventing things and though sometimes he fails he will succeed, he just keeps trying. He loves to study and play sports.",
+  activated_card:true },
+  { id:'',
+  name: "Ginjirotchi",
+  image:"https://tamagotchi.com/wp-content/uploads/ginjirotchi.jpg",
+  energy:100,
+  cost:500,
+  type:"UR",
+  race:"Nyan",
+  description: "Ginjirotchi is cheerful and full of energy, though also compassionate. He loves watching dramatic movies.",
+  activated_card:true}]
+
+ // clickableCards!:CardInt[];
+
+
+    constructor(private dialogRef: MatDialogRef<InitialCardChooserComponent>, private api: ApiService, private logins:LoginService,private Scard:seleced_Card_S) {
+      
+      //console.log(this.cards)
+    }
+    ngOnInit(): void {
+      this.api.getplayerCards(this.logins.getcorreo()).subscribe(data => {
+        console.log(data)
+        this.cards = data 
+      });
+      this.close()  
+      
+
+    }
+
+    close() {
+      if (this.cardsChosen==2){
+        this.dialogRef.close();}
+      else{
+        this.cardsChosen+=1
+        this.api.playerchoseCard(this?.Scard.getcard(),this.logins.getid())
+        this.api.getplayerCards(this.logins.getcorreo()).subscribe(data => {
+          console.log(data)
+          this.cards = data 
+        });
+        this.api.getchoosingcard(this.logins.getcorreo()).subscribe(data=>{
+          this.clickableCards=data
+        })
+      }
+    }
+    
     
   
     
