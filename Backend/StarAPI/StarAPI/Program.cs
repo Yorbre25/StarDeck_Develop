871f4470d2c9f6b8577c277e1f8f7c552f65547d
Nasser <neyss08@hotmail.com>
2023-04-26 18:49:00 -0600

    add cors restrictions

--- Backend/StarAPI/StarAPI/Program.cs
+++ Backend/StarAPI/StarAPI/Program.cs
@@ -9,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        buildr =>
+        {
+            buildr.WithOrigins("http://localhost:4200", "http://localhost:7023")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
