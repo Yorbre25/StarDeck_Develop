9b39362e3fde4a29d5ab5776bd2dbf6cba7d4566
adri <62965634+cuadriante@users.noreply.github.com>
2023-05-14 20:00:02 -0600

    Add: game setup turn, deck, bet and buttons

--- Frontend/src/app/components/views/game-setup/game/game.component.ts
+++ Frontend/src/app/components/views/game-setup/game/game.component.ts
@@ -3,6 +3,7 @@ import { CardInt } from 'src/app/components/interfaces/card.interface';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from 'src/app/components/services/api.service';
 import { PlanetInterface } from 'src/app/components/interfaces/planet.interface';
+import { DeckInterface } from 'src/app/components/interfaces/deck.interface';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -11,10 +12,21 @@ import { PlanetInterface } from 'src/app/components/interfaces/planet.interface'
 export class GameComponent {
 
   cards!: CardInt[];
+  deck!: DeckInterface;
   planets!: PlanetInterface[];
+  bet!:number;
+  turn!:number;
+  totalTurns!:number;
+  remainingCards!:number;
+  timeRemaining!:number;
 
 
   constructor(private api: ApiService, private http: HttpClient) {
+    this.bet = 0;
+    this.turn = 1;
+    this.totalTurns = 8;
+    this.remainingCards = 18;
+    this.timeRemaining = 20;
 
     //console.log(this.cards)
   }
